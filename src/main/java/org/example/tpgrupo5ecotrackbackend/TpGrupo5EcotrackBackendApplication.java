50b3226ad54a0c6c173376aae186d6b506949b96
Jhoselin-Diaz <xvantx2@gmail.com>
2026-04-13 15:15:45 -0500

    se agrego proyecto

---
+++ src/main/java/org/example/tpgrupo5ecotrackbackend/TpGrupo5EcotrackBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.tpgrupo5ecotrackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGrupo5EcotrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpGrupo5EcotrackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/tpgrupo5ecotrackbackend/TpGrupo5EcotrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tpgrupo5ecotrackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpGrupo5EcotrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
