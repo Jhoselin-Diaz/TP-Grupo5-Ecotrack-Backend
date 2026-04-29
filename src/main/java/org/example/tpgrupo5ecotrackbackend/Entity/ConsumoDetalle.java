9e7168da4ddb2f847fa06c6c29f82c0ea70ade26
Alex <alexoswaldo20@hotmail.com>
2026-04-28 22:25:33 -0500

    Avance

---
+++ src/main/java/org/example/tpgrupo5ecotrackbackend/Entity/ConsumoDetalle.java
@@ -0,0 +1,26 @@
+package org.example.tpgrupo5ecotrackbackend.Entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "consumo_detalle")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConsumoDetalle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idDetalle;
+
+    private String clave;
+    private String valor;
+
+    @ManyToOne
+    @JoinColumn(name = "consumo_id")
+    @JsonBackReference
+    private Consumo consumo;
+}
