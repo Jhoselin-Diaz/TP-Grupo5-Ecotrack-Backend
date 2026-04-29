5ec7ff9a01f649313b0f69fdd0d37bddef3ce463
TuNombre <TuCorreoGitHub>
2026-04-28 21:49:35 -0500

    Avance - Mateo Rentería

---
+++ src/main/java/org/example/tpgrupo5ecotrackbackend/DTO/ConsumoDetalleDTO.java
@@ -0,0 +1,15 @@
+package org.example.tpgrupo5ecotrackbackend.DTO;
+
+import lombok.Data;
+
+
+@Data
+public class ConsumoDetalleDTO {
+
+
+    private String clave;
+    private String valor;
+
+
+}
+
--- src/main/java/org/example/tpgrupo5ecotrackbackend/DTO/UsuarioDTO.java
+++ src/main/java/org/example/tpgrupo5ecotrackbackend/DTO/UsuarioDTO.java
@@ -1,16 +1,18 @@
 package org.example.tpgrupo5ecotrackbackend.DTO;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UsuarioDTO {
+public class UsuarioResponseDTO {
+    private Long idUsuario;
     private String username;
     private String correo;
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    private String password;
+    private Boolean enabled;
+    private String rol;
 }
+
