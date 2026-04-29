696c821e6f4fc790bc0cbf5a96dc173aeabe555b
Cuts01 <149018151+Cuts01@users.noreply.github.com>
2026-04-28 21:52:45 -0500

    Create ConsumoRepository.java

---
+++ src/main/java/org/example/tpgrupo5ecotrackbackend/Repository/ConsumoRepository.java
@@ -0,0 +1,8 @@
+package org.example.tpgrupo5ecotrackbackend.Repository;
+
+import org.example.tpgrupo5ecotrackbackend.Entity.Consumo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsumoRepository extends JpaRepository<Consumo, Long> {
+
+}
