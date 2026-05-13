7f4f09bc7a20ef2797640c2eebccbf9cd4f0c60c
Cuts01 <149018151+Cuts01@users.noreply.github.com>
2026-05-12 21:12:23 -0500

    Update UsuarioController.java

--- src/main/java/org/example/tpgrupo5ecotrackbackend/Controller/UsuarioController.java
+++ src/main/java/org/example/tpgrupo5ecotrackbackend/Controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package org.example.tpgrupo5ecotrackbackend.Controller;
 
+
 import org.example.tpgrupo5ecotrackbackend.DTO.UsuarioResponseDTO;
 import org.example.tpgrupo5ecotrackbackend.DTO.UsuarioDTO;
 import org.example.tpgrupo5ecotrackbackend.Service.UsuarioService;
@@ -7,32 +8,50 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin(origins = {"http://localhost:9876", "http://localhost:4200"})
 public class UsuarioController {
 
-    private final UsuarioService usuarioService;
-    public UsuarioController(UsuarioService usuarioService) {
-        this.usuarioService = usuarioService;
-    }
 
-    @GetMapping("/lista")
-    public List<UsuarioResponseDTO> listar() {
-        return usuarioService.obtenerUsuarios();
-    }
+   private final UsuarioService usuarioService;
+   public UsuarioController(UsuarioService usuarioService) {
+       this.usuarioService = usuarioService;
+   }
 
-    @PostMapping("/inserta")
-    public ResponseEntity<UsuarioResponseDTO> insertar(@RequestBody UsuarioDTO dto) {
-        UsuarioResponseDTO usuario = usuarioService.insertar(dto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
-    }
 
-    @DeleteMapping("/elimina/{id}")
-    public String eliminar(@PathVariable Long id) {
-        return usuarioService.eliminar(id);
-    }
+   @GetMapping("/lista")
+   public List<UsuarioResponseDTO> listar() {
+       return usuarioService.obtenerUsuarios();
+   }
+
+
+   @PostMapping("/inserta")
+   public ResponseEntity<UsuarioResponseDTO> insertar(@RequestBody UsuarioDTO dto) {
+       UsuarioResponseDTO usuario = usuarioService.insertar(dto);
+       return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+   }
+
+
+   @PutMapping("/actualiza/{id}")
+   public UsuarioResponseDTO actualizar(
+           @PathVariable Long id,
+           @RequestBody UsuarioDTO dto
+   ) {
+       return usuarioService.actualizar(id, dto);
+   }
+
+
+   @DeleteMapping("/elimina/{id}")
+   public String eliminar(@PathVariable Long id) {
+       return usuarioService.eliminar(id);
+   }
+
+
 
 
 }
